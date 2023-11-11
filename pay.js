c006d8a2d0d8b6ca007d0b8c3a3b02b86462d66d
react#1507 <127757116+reactor-coding@users.noreply.github.com>
2023-11-11 19:17:30 +0900

    修正 x8

---
+++ pay.js
@@ -0,0 +1,15 @@
+{
+  "name": "Buypass",
+  "short_name": "Buypass",
+  "icons": [
+    {
+      "src": "/pay/favicon.ico",
+      "sizes": "48x48",
+      "type": "image/x-icon"
+    }
+  ],
+  "serviceworker": {
+    "src": "/pay/sw.js",
+    "use_cache": false
+  }
+}
